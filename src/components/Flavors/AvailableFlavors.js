d1a4857865ea7a5c7aaa27a53b8fd2eff123b6cd
unknown <Amiram.ab@gmail.com>
2022-11-25 16:16:51 +0200

    local dummy data added and ploted inside flavors page with the help of flavorItem component

--- src/components/Flavors/AvailableFlavors.js
+++ src/components/Flavors/AvailableFlavors.js
@@ -1,7 +1,52 @@
 import React from 'react';
+import Card from '../UI/Card/Card';
+import FlavorItem from './FlavorItem/FlavorItem';
+import classes from './AvailableFlavors.module.css';
+const DUMMY_FLAVS = [
+    {
+        id: 'm1',
+        name: 'Blondie Ambition',
+        description: 'Cheesecake Ice Cream with Cheesecake Brownie Chunks',
+        price: 22.99,
+    },
+    {
+        id: 'm2',
+        name: 'Boom Chocolatta™ Cookie Core',
+        description: 'A german specialty!',
+        price: 16.5,
+    },
+    {
+        id: 'm3',
+        name: 'Bourbon Pecan Pie',
+        description: 'American',
+        price: 12.99,
+    },
+    {
+        id: 'm4',
+        name: 'Cheesecake Brownie',
+        description: 'Healthy...and green...',
+        price: 18.99,
+    },
+];
 
 const AvailableFlavors = () => {
-    return;
+    const flavorsList = DUMMY_FLAVS.map((flavor) => (
+        <FlavorItem
+            key={flavor.id}
+            id={flavor.id}
+            name={flavor.name}
+            description={flavor.description}
+            price={flavor.price}
+        />
+    ));
+
+    return (
+        <section className={classes.meals}>
+            <Card>
+                <ul>{flavorsList}</ul>
+            </Card>
+        </section>
+    );
 };
 
 export default AvailableFlavors;
---
+++ src/components/Flavors/FlavorItem/FlavorItem.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import classes from './FlavorItem.module.css';
+const FlavorItem = (props) => {
+    const price = `$${props.price.toFixed(2)}`;
+    return (
+        <li className={classes.flavor}>
+            <div>
+                <h3>{props.name}</h3>
+                <div className={classes.description}>{props.description}</div>
+                <div className={classes.price}>{price}</div>
+            </div>
+        </li>
+    );
+};
+
+export default FlavorItem;
