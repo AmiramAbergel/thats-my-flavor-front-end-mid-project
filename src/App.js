fb6cfdf42356feafdfa7289b36e24b8357723ff8
unknown <Amiram.ab@gmail.com>
2022-11-25 14:51:12 +0200

    Routes and pages added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import './App.css';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import Layout from './components/UI/Layout/Layout';
+import Home from './pages/Home';
+import Flavors from './pages/Flavors';
+import MapView from './pages/MapView';
+import RandFlavor from './pages/RandFlavor';
 
 function App() {
     return (
@@ -10,10 +14,10 @@ function App() {
                     path='/'
                     element={<Navigate replace to='/home'></Navigate>}
                 />
-                <Route path='/home' />
-                <Route path='/flavors' />
-                <Route path='/rand-flavor' />
-                <Route path='/map' />
+                <Route path='/home' element={<Home />} />
+                <Route path='/flavors' element={<Flavors />} />
+                <Route path='/rand-flavor' element={<RandFlavor />} />
+                <Route path='/map' element={<MapView />} />
             </Routes>
         </Layout>
     );
---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <main>
+            <h1>Welcome Page</h1>
+            <p>Learn more about our ice cream flavors</p>
+        </main>
+    );
+};
+
+export default Home;
